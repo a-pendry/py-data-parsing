52c0e079f5cdac22b21302ee026264a2a6bf5f7d
a-pendry <j.adam.pendry@gmail.com>
2020-08-07 17:32:38 -0500

    made basic script to parse data into a pandas dataframe object

--- parsing.py
+++ parsing.py
@@ -1,2 +1,11 @@
-msg = "Hello World"
-print(msg)
+import pandas as pd
+import os
+
+path = "./data/"
+
+for filename in os.listdir(path):
+    if filename.endswith(".csv"):
+        csv = pd.read_csv(os.path.join(path, filename))
+        continue
+    else:
+        continue
