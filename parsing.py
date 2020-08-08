3ed71dc73345abe196a011288b52d280018a7a56
a-pendry <j.adam.pendry@gmail.com>
2020-08-08 13:10:18 -0500

    finished initial goal of importing csv and exporting to a mysql table

--- parsing.py
+++ parsing.py
@@ -1,20 +1,32 @@
 import pandas as pd
 import os
 import mysql.connector
+import sqlalchemy
+from sqlalchemy.ext.declarative import declarative_base 
 
-mydb = mysql.connector.connect(
-    host = "localhost",
-    user = "root",
-    passwd = "mysqlisbetterthanyours!",
-    auth_plugin = "mysql_native_password",
-    database = "pyparsingdb"
-)
+
+engine = sqlalchemy.create_engine(
+    'mysql+mysqlconnector://root:mysqlisbetterthanyours!@localhost:3306/pyparsingdb',
+    echo=True)
 
 path = "./data/"
+
 
 for filename in os.listdir(path):
     if filename.endswith(".csv"):
         csv = pd.read_csv(os.path.join(path, filename))
+        connection = engine.connect()
+        tablename = filename.translate({ord(i): None for i in '.csv'})
+        try:
+            frame = csv.to_sql(tablename, connection, if_exists='fail')
+        except ValueError as vx:
+            print(vx)
+        except Exception as ex:
+            print(ex)
+        else:
+            print("Table %s created successfully."%tablename)   
+        finally:
+            connection.close()
         continue
     else:
         continue
